948f48aa1167c016555dcf6feb5dfbe21a9c9e9c
Alexander Swensen <alex.swensen@gmail.com>
2020-09-24 08:51:00 -0500

    setup global fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-import { Container } from 'react-bootstrap'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
    <div>
-     <Container>
-       <h2>hello world</h2>
-     </Container>
+       <h1>Hello World</h1>
    </div>
   )
 }
