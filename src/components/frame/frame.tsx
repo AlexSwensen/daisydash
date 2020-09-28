5eb3d7b18637f0de21e28f524ea6a6bf52f19d4c
Alexander Swensen <alex.swensen@gmail.com>
2020-09-28 18:38:47 -0500

    use child props

--- src/components/frame/frame.tsx
+++ src/components/frame/frame.tsx
@@ -1,15 +1,11 @@
 import { Navbar } from "../navbar/Navbar";
 import styles from "./frame.module.scss";
 
-export default function Frame() {
+export default function Frame({ children }) {
   return (
     <div className={styles.frameContainer}>
       <Navbar></Navbar>
-      <div className={styles.frameContent}>
-        <div className={styles.block}></div>
-        <div className={styles.block}></div>
-        <div className={styles.block}></div>
-      </div>
+      <div className={styles.frameContent}>{children}</div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import Frame from "../components/frame/frame";
 import styles from "../styles/Home.module.scss";
+import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <div>
+    <Fragment>
       <Frame></Frame>
-    </div>
+    </Fragment>
   );
 }
