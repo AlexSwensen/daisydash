6b9f9c2db5c5a1dfd1208a9e2dcf27658c4ef21e
Alexander Swensen <alex.swensen@gmail.com>
2020-09-27 20:12:04 -0500

    flex layout

--- src/components/frame/frame.tsx
+++ src/components/frame/frame.tsx
@@ -1,23 +1,14 @@
 import { Navbar } from "../navbar/Navbar";
 import styles from "./frame.module.scss";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid palevioletred;
-  color: palevioletred;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`;
 
 export default function Frame() {
   return (
     <div className={styles.frameContainer}>
       <Navbar></Navbar>
       <div className={styles.frameContent}>
-        <h1>Coming soon! 🙂</h1>
-        <Button>foobar</Button>
+        <div className={styles.block}></div>
+        <div className={styles.block}></div>
+        <div className={styles.block}></div>
       </div>
     </div>
   );
